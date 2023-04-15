9fe16b7c6e0be571bd0d9d4df76b1c90ab2ec31b
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-04-15 22:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	var a = document.getElementById("firstName").value;
+	var b = document.getElementById("lastName").value;
+	
+	alert(a+" "+b);
 }
